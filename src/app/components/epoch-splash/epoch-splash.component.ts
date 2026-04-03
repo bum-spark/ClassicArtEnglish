304b78cf4a4c9e35d8097212f4bb26aa2db440e9
Jordan Cazares <l.caej040411@itses.edu.mx>
2026-04-03 15:07:30 -0600

    Mejora splash

--- src/app/components/epoch-splash/epoch-splash.component.ts
+++ src/app/components/epoch-splash/epoch-splash.component.ts
@@ -23,10 +23,12 @@ export class EpochSplashComponent implements OnInit, OnDestroy {
   private seenEpochs = new Set<string>();
 
   isShowing = false;
+  isLeaving = false;
   currentName = '';
   currentYears = '';
   currentImages: string[] = [];
-  private dismissTimeout: any;
+  activeImageIndex = 0;
+  private slideInterval: any;
 
   epochData: EpochSplashData[] = [
     {
@@ -37,6 +39,7 @@ export class EpochSplashComponent implements OnInit, OnDestroy {
         'https://upload.wikimedia.org/wikipedia/commons/thumb/d/de/Colosseo_2020.jpg/1280px-Colosseo_2020.jpg',
         'https://upload.wikimedia.org/wikipedia/commons/thumb/c/c1/Pompeii_-_Casa_dei_Vettii_-_Pentheus.jpg/960px-Pompeii_-_Casa_dei_Vettii_-_Pentheus.jpg',
         'https://upload.wikimedia.org/wikipedia/commons/thumb/3/32/Peter_Paul_Rubens_-_The_Judgment_of_Paris_-_WGA20307.jpg/1280px-Peter_Paul_Rubens_-_The_Judgment_of_Paris_-_WGA20307.jpg',
+        'https://upload.wikimedia.org/wikipedia/commons/thumb/8/82/Cleomene_di_Apollodoro%2C_venere_medici%2C_I_secolo_ac_ca.jpg/500px-Cleomene_di_Apollodoro%2C_venere_medici%2C_I_secolo_ac_ca.jpg',
       ]
     },
     {
@@ -46,6 +49,7 @@ export class EpochSplashComponent implements OnInit, OnDestroy {
       images: [
         'https://www.art-theoria.com/wp-content/uploads/2015/03/Giotto-Lamentation.jpg',
         'https://upload.wikimedia.org/wikipedia/commons/thumb/4/49/%22The_School_of_Athens%22_by_Raffaello_Sanzio_da_Urbino.jpg/1920px-%22The_School_of_Athens%22_by_Raffaello_Sanzio_da_Urbino.jpg',
+        'https://upload.wikimedia.org/wikipedia/commons/thumb/6/69/Bayeux_Tapestry_scene57_Harold_death.jpg/1280px-Bayeux_Tapestry_scene57_Harold_death.jpg',
       ]
     },
     {
@@ -56,6 +60,7 @@ export class EpochSplashComponent implements OnInit, OnDestroy {
         'https://upload.wikimedia.org/wikipedia/commons/thumb/0/0b/Sandro_Botticelli_-_La_nascita_di_Venere_-_Google_Art_Project_-_edited.jpg/1920px-Sandro_Botticelli_-_La_nascita_di_Venere_-_Google_Art_Project_-_edited.jpg',
         'https://upload.wikimedia.org/wikipedia/commons/thumb/4/48/The_Last_Supper_-_Leonardo_Da_Vinci_-_High_Resolution_32x16.jpg/1920px-The_Last_Supper_-_Leonardo_Da_Vinci_-_High_Resolution_32x16.jpg',
         'https://www.datocms-assets.com/103094/1688661508-1506165873655660-artemisia-giuditta-principale.jpg?auto=format%2Ccompress&cs=srgb&max-w=800',
+        'https://upload.wikimedia.org/wikipedia/commons/thumb/e/ec/Mona_Lisa%2C_by_Leonardo_da_Vinci%2C_from_C2RMF_retouched.jpg/800px-Mona_Lisa%2C_by_Leonardo_da_Vinci%2C_from_C2RMF_retouched.jpg',
       ]
     },
     {
@@ -65,6 +70,7 @@ export class EpochSplashComponent implements OnInit, OnDestroy {
       images: [
         'https://upload.wikimedia.org/wikipedia/commons/thumb/5/54/Claude_Monet%2C_Impression%2C_soleil_levant.jpg/1280px-Claude_Monet%2C_Impression%2C_soleil_levant.jpg',
         'https://upload.wikimedia.org/wikipedia/commons/thumb/e/ea/Van_Gogh_-_Starry_Night_-_Google_Art_Project.jpg/1280px-Van_Gogh_-_Starry_Night_-_Google_Art_Project.jpg',
+        'https://upload.wikimedia.org/wikipedia/commons/thumb/0/0a/The_Great_Wave_off_Kanagawa.jpg/1280px-The_Great_Wave_off_Kanagawa.jpg',
       ]
     },
     {
@@ -75,6 +81,7 @@ export class EpochSplashComponent implements OnInit, OnDestroy {
         'https://www.edvardmunch.org/assets/img/paintings/the-scream.jpg',
         'https://cdn.culturagenial.com/es/imagenes/cuadro-guernica-de-pablo-picasso-og.jpg',
         'https://www.moma.org/media/W1siZiIsIjMxODI0MiJdLFsicCIsImNvbnZlcnQiLCItcXVhbGl0eSA5MCAtcmVzaXplIDIwMDB4MTQ0MFx1MDAzZSJdXQ.jpg?sha=f1e923ce509ba9e6',
+        'https://m.media-amazon.com/images/I/71kbG+cExzL.jpg',
       ]
     },
     {
@@ -84,6 +91,7 @@ export class EpochSplashComponent implements OnInit, OnDestroy {
       images: [
         'https://www.cnet.com/a/img/resize/e0a5ae097ce1143e9a9d7e017f685e079e46f8ba/hub/2022/09/28/905de384-f115-42ed-9e8a-07746675658b/dall-e-parachuting-elephant.jpg?auto=webp&fit=crop&height=900&width=1200',
         'https://queue-it.com/media/ss1dxknh/bored-apes.jpg',
+        'https://upload.wikimedia.org/wikipedia/commons/thumb/c/c4/Spacewar%21-PDP-1-20070512.jpg/1280px-Spacewar%21-PDP-1-20070512.jpg',
       ]
     }
   ];
@@ -108,7 +116,7 @@ export class EpochSplashComponent implements OnInit, OnDestroy {
 
   ngOnDestroy(): void {
     this.routerSub?.unsubscribe();
-    clearTimeout(this.dismissTimeout);
+    this.stopSlideshow();
   }
 
   private checkSplash(url: string): void {
@@ -116,24 +124,38 @@ export class EpochSplashComponent implements OnInit, OnDestroy {
     if (!epoch) return;
     if (this.seenEpochs.has(epoch.name)) return;
 
-    // Mark as seen
     this.seenEpochs.add(epoch.name);
 
-    // Show splash
     this.currentName = epoch.name;
     this.currentYears = epoch.years;
     this.currentImages = epoch.images;
+    this.activeImageIndex = 0;
+    this.isLeaving = false;
     this.isShowing = true;
+    document.body.style.overflow = 'hidden';
 
-    // Auto-dismiss after 4 seconds
-    clearTimeout(this.dismissTimeout);
-    this.dismissTimeout = setTimeout(() => {
-      this.dismiss();
-    }, 4000);
+    this.startSlideshow();
+  }
+
+  private startSlideshow(): void {
+    this.stopSlideshow();
+    this.slideInterval = setInterval(() => {
+      this.activeImageIndex = (this.activeImageIndex + 1) % this.currentImages.length;
+    }, 3000);
+  }
+
+  private stopSlideshow(): void {
+    clearInterval(this.slideInterval);
   }
 
   dismiss(): void {
-    this.isShowing = false;
-    clearTimeout(this.dismissTimeout);
+    this.isLeaving = true;
+    // Wait for fade-out animation before hiding
+    setTimeout(() => {
+      this.isShowing = false;
+      this.isLeaving = false;
+      document.body.style.overflow = '';
+      this.stopSlideshow();
+    }, 800);
   }
 }
